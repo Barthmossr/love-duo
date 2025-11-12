137a02039211396477c116c3f5b951cc07452308
Barthmossr <barthmossr@gmail.com>
2025-11-12 19:02:28 -0300

    chore: add eslint jest rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import typescriptPlugin from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
 import importPlugin from 'eslint-plugin-import'
+import jestPlugin from 'eslint-plugin-jest'
 
 export default [
   {
@@ -34,6 +35,20 @@ export default [
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/no-unused-vars': 'error'
+    }
+  },
+  {
+    files: ['tests/**/*.{ts,tsx}'],
+    plugins: {
+      jest: jestPlugin
+    },
+    languageOptions: {
+      globals: jestPlugin.environments.globals.globals
+    },
+    rules: {
+      'jest/consistent-test-it': ['error', { fn: 'it', withinDescribe: 'it' }],
+      'jest/prefer-lowercase-title': ['error', { ignore: ['describe'] }],
+      'jest/valid-title': ['error', { mustMatch: { it: '^should\\s' } }]
     }
   }
 ]
