4349c39e1115b74bc85e4fbbb3bf54330e128a60
Barthmossr <barthmossr@gmail.com>
2025-11-14 15:48:26 +1300

    test: ensure PairCode screen does not navigates when invalid

---
+++ tests/unit/onboarding/screens/paircode.spec.tsx
@@ -0,0 +1,13 @@
+import { fireEvent, render } from '@testing-library/react-native'
+
+import { PairCode } from '@/onboarding/screens/PairCode'
+
+describe('PairCode screen', () => {
+  it('should not navigate when invalid', () => {
+    const navigate = jest.fn()
+    const { getByTestId } = render(<PairCode navigation={{ navigate }} />)
+    fireEvent.changeText(getByTestId('paircode-input'), '123456')
+    fireEvent.press(getByTestId('paircode-next'))
+    expect(navigate).not.toHaveBeenCalled()
+  })
+})
