a0a4f9d46d875d304ea660640d9fc348fe2bd13c
Barthmossr <barthmossr@gmail.com>
2025-11-14 15:46:22 +1300

    test: adjust app tests

--- tests/unit/app/app.spec.tsx
+++ tests/unit/app/app.spec.tsx
@@ -1,10 +1,23 @@
-import { render } from '@testing-library/react-native'
+import { fireEvent, render } from '@testing-library/react-native'
 
-import { App } from '@/app'
+import { App } from '@/app/App'
 
 describe('<App />', () => {
-  it('should render intro text', () => {
+  it('should render start screen', () => {
     const { getByText } = render(<App />)
-    getByText('Open up App.tsx to start working on your app!')
+    getByText('Nossa História')
+    getByText('Um espaço especial para o casal')
+  })
+
+  it('should show actions', () => {
+    const { getByText } = render(<App />)
+    getByText('Entrar com Código')
+    getByText('Criar Novo Casal')
+  })
+
+  it('should trigger navigation actions', () => {
+    const { getByTestId } = render(<App />)
+    fireEvent.press(getByTestId('start-join'))
+    fireEvent.press(getByTestId('start-create'))
   })
 })
