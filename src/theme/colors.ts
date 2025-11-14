50353aa6a3ad051fea73e25b0942df066176cd9a
Barthmossr <barthmossr@gmail.com>
2025-11-14 15:42:22 +1300

    feat: add theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+const colors = {
+  accent: '#feaf83',
+  accentLight: '#ffd8be',
+  accentSoft: 'rgba(254,175,131,0.15)',
+  background: '#feaf83',
+  primary: '#ff9da5',
+  neutral: '#f5f4f8',
+  secondary: '#c1a9ee',
+  base: '#fcf8f1',
+  textDark: '#20212b',
+  textOnPrimary: '#ffffff'
+}
+
+export { colors }
