d830580d958239727a5477e05892b36eb3d53ee5
Barthmossr <barthmossr@gmail.com>
2025-11-14 15:49:53 +1300

    test: ensure register navigates to confirm

--- tests/unit/onboarding/screens/register.spec.tsx
+++ tests/unit/onboarding/screens/register.spec.tsx
@@ -9,4 +9,11 @@ describe('CreateCouple screen', () => {
     )
     getByText('Create Couple')
   })
+
+  it('should navigate to confirm', () => {
+    const navigate = jest.fn()
+    const { getByTestId } = render(<CreateCouple navigation={{ navigate }} />)
+    fireEvent.press(getByTestId('create-next'))
+    expect(navigate).toHaveBeenCalledWith('Confirm')
+  })
 })
