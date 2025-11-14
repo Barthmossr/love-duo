abad75b03d410280ba6dd719ba24e9495aefa1ea
Barthmossr <barthmossr@gmail.com>
2025-11-14 15:50:14 +1300

    test: ensure Start screen navigates to pair code

---
+++ tests/unit/onboarding/screens/start.spec.tsx
@@ -0,0 +1,12 @@
+import { fireEvent, render } from '@testing-library/react-native'
+
+import { Start } from '@/onboarding/screens/Start'
+
+describe('Start screen', () => {
+  it('should navigate to pair code', () => {
+    const navigate = jest.fn()
+    const { getByTestId } = render(<Start navigation={{ navigate }} />)
+    fireEvent.press(getByTestId('start-join'))
+    expect(navigate).toHaveBeenCalledWith('PairCode')
+  })
+})
