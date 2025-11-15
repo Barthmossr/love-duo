4e49d530170fd31dc20710a91ac7acbade2e7693
Barthmossr <barthmossr@gmail.com>
2025-11-15 20:56:01 -0300

    test: ensure Welcome renders subtitle correctly

--- tests/unit/components/Welcome.test.tsx
+++ tests/unit/components/Welcome.test.tsx
@@ -7,4 +7,9 @@ describe('Welcome', () => {
     const { getByText } = render(<Welcome />)
     expect(getByText('Nossa História')).toBeDefined()
   })
+
+  it('should render subtitle correctly', () => {
+    const { getByText } = render(<Welcome />)
+    expect(getByText('Um espaço especial para o casal')).toBeDefined()
+  })
 })
